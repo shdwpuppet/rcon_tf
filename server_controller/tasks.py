f52881bb56efec82d09818c4d93e3b6fa33e783f
Alex Williams <shdw.puppet@gmail.com>
2016-06-07 18:01:25 -0400

    added celery task and basic rcon command send and check views

--- server_controller/tasks.py
+++ server_controller/tasks.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 from celery import shared_task
+from valve.source.rcon import RCON
 
 
 @shared_task
+def rcon_send(server, command):
+    with RCON((server.ip, server.port), server.password) as rcon:
+        return rcon(command)
+
--- server_controller/views.py
+++ server_controller/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import JsonResponse
+from .models Server
+from celery.result import AsyncResult
+import simplejson
 
 def dashboard(request):
     return HttpResponse('dashboard')
+
+
+def send_command(request):
+    if request.method == 'POST':
+        if 'data' not in request.POST:
+            return HttpResponse(status=400)
+        else:
+            data = simplejson.JSONDecoder().decode(request.POST['data'])
+            server = Server.objects.get(uuid=data['uuid'])
+            proc_id = server.execute_or_none(request.user, data['command'])
+            if server.execute_or_none(request.user, data['command']): # execute_or_none returns a celery task id if
+                return JsonResponse({'proc_id': proc_id})
+            else:
+                return HttpResponse(status=403)
+
+    return HttpResponse(status=405)
+
+def check_proc(request):
+    if request.method == 'POST'
+        if 'proc_id' in request.POST:
+            async_result = AsyncResult(request.POST['proc_id'])
+            try:
+                result = async_result.get(timeout=5, propagate=False)
+            except TimeoutError:
+                result = None
+            status = async_result.status
+            if isinstance(result, Exception):
+                return JsonResponse({
+                    'status': status,
+                    'error': str(result)
+                })
+            else:
+                return JsonResponse({
+                    'status': status,
+                    'result': result
+                })
+        else:
+            return HttpResponse(status=400)
+    else:
+        return HttpResponse(status = 405)
